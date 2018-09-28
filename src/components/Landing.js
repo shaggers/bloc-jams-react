1228fca53956330dc88af680624511aee0eebcbf
Cameron <camtl66@gmail.com>
2018-09-28 16:35:53 -0400

    checkpoint 4 work

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 const Landing = () => (
 	<section className='landing'>
-		Landing page goes here.
+		<h1 className='hero-title'>Turn the music up!</h1>
+
+		<section className='selling-points'>
+			<div className='point'>
+				<h2 className='point-title'>Choose your music</h2>
+				<p className='point-description'>The world is full of awesome music.</p>
+			</div>
+			<div className='point'>
+				<h2 className='point-title'>Unlimited streaming ad-free</h2>
+				<p className='point-description'>No arbitrary limits. No distractions.</p>
+			</div>
+			<div className='point'>
+				<h2 className='class-title'>Mobile enabled</h2>
+				<p className='class-description'>Listen to your music on the go</p>
+			</div>
+		</section>
 	</section>
 );
 
