37fb4000fae1673b3b5167d9ff5fa6254909ed1f
Cameron <camtl66@gmail.com>
2018-10-07 16:10:55 -0400

    checkpoint7

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,11 +10,42 @@ class Album extends Component {
 		});
 
 		this.state = {
-			album: album
-		}
+			album: album,
+			currentSong: album.songs[0],
+			isPlaying: false
+		};
+
+		this.audioElement = document.createElement('audio');
+		this.audioElement.src = album.songs[0].audioSrc;
+
 	}
 
-	render() {
+		play() {
+			this.audioElement.play();
+			this.setState({ isPlaying: true });
+		}
+
+		pause() {
+			this.audioElement.pause();
+			this.setState({ isPlaying: false });
+		}
+
+		setSong(song) {
+			this.audioElement.src = song.audioSrc;
+			this.setState({ currentSong: song });
+		}
+
+		handleSongClick(song) {
+			const isSameSong = this.state.currentSong === song;
+			if (this.state.isPlaying && isSameSong) {
+				this.pause();
+			} else {
+				if (!isSameSong) { this.setSong(song); }
+				this.play();
+			}
+		}
+
+		render() {
 		return (
 			<section className="album">
 				<section id="album-info">
@@ -38,7 +69,7 @@ class Album extends Component {
 					<tbody>
 						{
 							this.state.album.songs.map( (song, index) =>
-								<tr className="song" key="index">
+								<tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
 									<td>{index + 1}</td>
 									<td>{song.title}</td>
 									<td>{song.duration}</td>
